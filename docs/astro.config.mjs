2db6e9ddcd142d9cd0b1f46d30f1f826b56dad9f
Brendan Ashworth <brendan.ashworth@me.com>
2025-08-27 20:40:57 -0700

    more than satellite

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -53,7 +53,7 @@ export default defineConfig({
 					items: [
 						{ label: 'Geoprocessing from QGIS', slug: 'guides/geoprocessing-from-qgis' },
 						{ label: 'Visualizing point clouds', slug: 'guides/visualizing-point-clouds-las-laz' },
-						{ label: 'Satellite basemaps', slug: 'guides/switching-basemaps-satellite-or-traditional-vector' },
+						{ label: 'Switching basemaps', slug: 'guides/switching-basemaps-satellite-or-traditional-vector' },
 						{ label: 'Embedding into websites', slug: 'guides/embedding-maps-into-websites' },
 					],
 				},
